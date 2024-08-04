ef25b66e5c97e57f92c5993cba1aa45aeacf5eeb
josigar <104169012+josigar123@users.noreply.github.com>
2024-08-04 13:52:37 +0200

    add mongodb config

--- backend/url-shortener.api/url-shortener.api/Program.cs
+++ backend/url-shortener.api/url-shortener.api/Program.cs
@@ -1,27 +1,9 @@
-
 using MongoDB.Driver;
 using MongoDB.Bson;
 
-const string connectionUri = "mongodb+srv://Cluster68311:<password>@cluster68311.m06jkmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster68311";
-
-var settings = MongoClientSettings.FromConnectionString(connectionUri);
-
-// Set the ServerApi field of the settings object to set the version of the Stable API on the client
-settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-
-// Create a new client and connect to the server
-var client = new MongoClient(settings);
-
-// Send a ping to confirm a successful connection
-try {
-    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (Exception ex) {
-    Console.WriteLine(ex);
-}
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+InitializeMongoClient(builder);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,3 +19,27 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+return;
+
+void InitializeMongoClient(WebApplicationBuilder builder)
+{
+    var mongoDbSection = builder.Configuration.GetSection("MongoDB");
+    var username = mongoDbSection["Username"];
+    var password = mongoDbSection["Password"];
+
+    var connectionUri = $"mongodb+srv://{username}:{password}@cluster68311.m06jkmr.mongodb.net/?retryWrites=true&w=majority&appName={username}";
+    var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+    settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+    
+    var client = new MongoClient(settings);
+    
+    try {
+        var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+        Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (Exception ex) {
+        Console.WriteLine(ex);
+    }
+
+    builder.Services.AddSingleton(client);
+}
