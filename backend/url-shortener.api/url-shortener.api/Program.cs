ccbf3079879c118daf5c24f4687eb78f55f3a277
josigar <104169012+josigar123@users.noreply.github.com>
2024-08-04 13:01:38 +0200

    mongodb setup

--- backend/url-shortener.api/url-shortener.api/Program.cs
+++ backend/url-shortener.api/url-shortener.api/Program.cs
@@ -1,3 +1,26 @@
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+const string connectionUri = "mongodb+srv://Cluster68311:<password>@cluster68311.m06jkmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster68311";
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try {
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+    Console.WriteLine(ex);
+}
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
